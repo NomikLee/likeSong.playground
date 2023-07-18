5b7b4582a2dc8390f34a8b5c200d053ada6db2a9
Nomik Lee <s79318@icloud.com>
2023-07-18 22:30:07 +0800

    main code

---
+++ Contents.swift
@@ -0,0 +1,81 @@
+import UIKit
+
+func babyPregnant()  {
+    print("Baby I’m pregnant she said")
+    print("And I saw my whole life")
+    print("Flash before my eyes")
+    print("So much for planning ahead")
+    print("We gon’ be all right")
+    print("There won’t be a right time")
+    print("Oh, I said")
+    print("I’m way too young")
+    print("I’m just a kid")
+    print("I can’t raise one")
+    print("It feels like my life is over")
+    print("Feels like my future crushed")
+    print("And I’m gonna miss the times when")
+    print("It was just the two of us")
+    print("I don’t ever want to let go")
+    print("Of everything that I love")
+    print("It sure feels like dying")
+}
+
+func sayingGoodbye(change iyrics: String)  {
+    print("Saying goodbye")
+    print("To my \(iyrics) life")
+}
+
+
+func daddySleep()  {
+    print("Daddy I can’t sleep she said")
+    print("Can you leave the light on")
+    print("And please sing me my song")
+    print("And I can’t help but think to myself")
+    print("Those green eyes and brown curls")
+    print("Turned into my whole world")
+    print("She’s growing up so fast")
+    print("If only I could make it last")
+    print("It feels like my life is over")
+    print("Feels like my future crushed")
+    print("Cause my baby’s getting older")
+    print("Tomorrow she’ll be all grown up")
+    print("I don’t ever want to let go")
+    print("Of everything that I love")
+    print("I turn off the lights")
+}
+
+func andGoodnight(change iyrics: String)  {
+    print("And say goodnight")
+    print("To my \(iyrics) life")
+}
+
+func packBags()  {
+    print("Packed the bags and ready to go")
+    print("We look at each other")
+    print("She looks like her mother")
+    print("Off to chase dreams of her own")
+    print("She cries in the backseat")
+    print("As we wave at the taxi")
+    print("Oh, our baby is moving out")
+    print("Leaving this home an empty house")
+    print("It feels like my life is over")
+    print("Feels like my future crushed")
+    print("And I’m gonna miss the times when")
+    print("It was just the three of us")
+    print("I don’t ever want to let go")
+    print("Of everything that I love")
+    print("Sure, feels like dying")
+}
+
+func flashEyes()  {
+    print("Baby I’m pregnant she said")
+    print("And I saw my whole life")
+    print("Flash before my eyes")
+}
+
+babyPregnant()
+sayingGoodbye(change: "miserable")
+daddySleep()
+andGoodnight(change: "miserable")
+packBags()
+flashEyes()
